a9cc7a805accb0d195faca826b934cf68e79ffa0
Дмитрий Фют <101337758+DmitriFyut@users.noreply.github.com>
2022-09-07 01:19:01 +0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,23 @@
+valid_email = "dmitryfyut@yandex.ru"
+valid_password = "qwerty"
+project_url = 'https://www.labirint.ru/'
+favorites_url = 'https://www.labirint.ru/cabinet/putorder/'
+cart_url = 'https://www.labirint.ru/cart/'
+compare_url = 'https://www.labirint.ru/compare/'
+support_url = 'https://www.labirint.ru/support/'
+help_url = 'https://www.labirint.ru/help/'
+checkout_page = 'https://www.labirint.ru/basket/checkout/'
+discount_code = '640E-4AA3-****'
+email = 'test@test.test'
+login = 'Дмитрий'
+valid_phone = '+79012723793'
+
+social_network = {
+    'vk': 'https://vk.com/labirint_ru',
+    'vk.deti': 'https://vk.com/labirintdeti',
+    'youtube': 'https://www.youtube.com/user/labirintruTV',
+    'ok': 'https://ok.ru/labirintru',
+    'zen': 'https://zen.yandex.ru/labirintru',
+    'telegram': 'https://t.me/labirintru',
+    'tiktok': 'https://www.tiktok.com/@labirintru'
+}
